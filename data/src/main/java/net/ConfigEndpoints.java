399d8ed1888a28e7e4cdc1853d4bb4934868c790
zoragy <zogovicbojan@gmail.com>
2016-02-23 11:59:29 +0100

    Initial commit

---
+++ data/src/main/java/net/ConfigEndpoints.java
@@ -0,0 +1,7 @@
+package net;
+
+public interface ConfigEndpoints {
+    String URL_BASE = "http://192.168.166.22:8080/useraccount/";
+    String HEADER_APP_ID = "";
+    String HEADER_REST_API_KEY = "";
+}
